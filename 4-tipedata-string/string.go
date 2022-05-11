b32e46292a521179c5f26394ee5c526d38683d04
jokojr97 <joko.riyadi97@gmail.com>
2022-05-11 13:23:19 +0700

    update tipedata

---
+++ 4-tipedata-string/string.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world func len")
+	fmt.Println(len("hello world"))
+	fmt.Println("hello world"[3])
+	fmt.Println("ini adalah tipedata string")
+}
